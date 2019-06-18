f67d617f317832b9f156a42d03ac7962d04cca31
Gustavo Ubatuba <gustavo.ubatuba@gmail.com>
2019-06-18 00:42:55 -0300

    more models

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ db = peewee.MySQLDatabase(
 	host='127.0.0.1', 
 	user='root', 
 	passwd='',
-	database='capitolio')
+	database='dbname')
 
 class BaseModel(Model):
     class Meta:
         database = db
 		
-class clp_tb_empresas(BaseModel):
-	id_empresa = IntegerField(unique=True)
+class Empresa(BaseModel):
+	id_empresa = PrimaryKeyField()
 	nome_empresa = TextField()
 	razao_social = TextField()
 	endereco = TextField()
@@ -35,10 +35,13 @@ class clp_tb_empresas(BaseModel):
 	contato_financ_celular = TextField()
 	contato_financ_email = TextField()
 	tipo_envio = IntegerField(default=0)
-
-class clp_tb_clientes(BaseModel):
-	id_cliente = AutoField()
-	empresa = ForeignKeyField(clp_tb_empresas)
+	
+	class Meta:
+		table_name = 'clp_tb_empresas'
+		
+class Cliente(BaseModel):
+	id_cliente = PrimaryKeyField()
+	id_empresa = IntegerField()
 	nome_cliente = TextField()
 	titulo_cliente = TextField()
 	cargo_cliente = TextField()
@@ -50,10 +53,58 @@ class clp_tb_clientes(BaseModel):
 	observacoes_cliente = TextField()
 	tipo_envio = IntegerField()
 	Habilitado = IntegerField(default=1)
+	empresa = ForeignKeyField(Empresa, db_column='id_empresa')
+	
+	class Meta:
+		table_name = 'clp_tb_clientes'
 
+class Produto(BaseModel):
+	id_produto = PrimaryKeyField()
+	nome_produto = TextField()
+	tipo = TextField()
+	template = TextField()
+	
+	class Meta:
+		table_name = 'clp_tb_produtos'
 
+class Informe(BaseModel):
+	id_informe = PrimaryKeyField()
+	id_produto = IntegerField()
+	data = TextField()
+	remetente = TextField()
+	titulo = TextField()
+	corpo = TextField()
+	template = TextField()
+	id_usuario = IntegerField()
+	produto = ForeignKeyField(Produto, db_column='id_produto')
+	
+	class Meta:
+		table_name = 'clp_tb_informes'
 
+class InformeLog(BaseModel):
+	id_informe = IntegerField()
+	id_cliente = IntegerField()
+	status = TextField()
+	tentativas = IntegerField()
+	cliente = ForeignKeyField(Cliente, db_column='id_cliente')
+	informe = ForeignKeyField(Informe, db_column='id_informe')
+	
+	class Meta:
+		primary_key = False
+		table_name = 'clp_tb_informe_log'
 
-for clientes in clp_tb_clientes.select():
-    print(clientes.id_cliente, clientes.nome_cliente, clientes.empresa.nome_empresa)
+#for cliente in Cliente.select():
+	#print(cliente.nome_cliente)
 
+#for informe in Informe.select():
+#	print(informe.titulo, informe.remetente, informe.produto.nome_produto)
+
+#for cliente in Cliente.select():
+#	print('ID [{}] Nome [{}] Empresa[{}]'.format(cliente.id_cliente,cliente.nome_cliente, cliente.empresa.nome_empresa))
+a = 0
+for informeLog in InformeLog.select():
+	print(informeLog.tentativas, informeLog.status, informeLog.cliente.nome_cliente, informeLog.informe.titulo)
+	a = a + 1
+	print(a)
+	print(informeLog.tentativas)
+print(a)
