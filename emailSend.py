866f418aaf3e92404a9d0f1a6b6d9b555bb40334
Gustavo Ubatuba <gustavo.ubatuba@gmail.com>
2019-06-25 00:14:04 -0300

    update apos o envio

--- emailSend.py
+++ emailSend.py
@@ -1,3 +1,4 @@
+import datetime
 import secureProperties
 import os
 import boto3
@@ -7,11 +8,10 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 class emailSend():
-    def send_single_email(destinatario, titulo, corpo):
+    def send_single_email(destinatario, titulo, corpo, title_image, title_image_path, bottom_image, bottom_image_path):
+        print(datetime.datetime.now())
         client = boto3.client('ses',region_name=secureProperties.AWS_REGION)
         
-        ATTACHMENT = "../images/titulo.png"
-        # Create a multipart/mixed parent container.
         msg = MIMEMultipart('mixed')
         # Add subject, from and to lines.
         msg['Subject'] = titulo 
@@ -29,18 +29,29 @@ class emailSend():
         msg_body.attach(htmlpart)
 
         # Define the attachment part and encode it using MIMEApplication.
-        att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+        title_att = MIMEApplication(open(title_image_path, 'rb').read())
 
         # Add a header to tell the email client to treat this part as an attachment,
         # and to give the attachment a name.
-        att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+        title_att.add_header('Content-Disposition','attachment',filename=title_image)
+        title_att.add_header('X-Attachment-Id', '0')
+        title_att.add_header('Content-ID', '<0>')
 
+        # Define the attachment part and encode it using MIMEApplication.
+        bottom_att = MIMEApplication(open(bottom_image_path, 'rb').read())
+
+        # Add a header to tell the email client to treat this part as an attachment,
+        # and to give the attachment a name.
+        bottom_att.add_header('Content-Disposition','attachment',filename=bottom_image)
+        bottom_att.add_header('X-Attachment-Id', '1')
+        bottom_att.add_header('Content-ID', '<1>')
         # Attach the multipart/alternative child container to the multipart/mixed
         # parent container.
         msg.attach(msg_body)
 
         # Add the attachment to the parent container.
-        msg.attach(att)
+        msg.attach(title_att)
+        msg.attach(bottom_att)
         # Try to send the email.
         try:
             #Provide the contents of the email.
@@ -56,7 +67,7 @@ class emailSend():
             )
         # Display an error if something goes wrong.	
         except ClientError as e:
-            print(e.response['Error']['Message'])
+            return False
         else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
+            print(datetime.datetime.now())
+            return True
--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class BaseModel(Model):
 		
 class Empresa(BaseModel):
 	id = PrimaryKeyField(db_column='id_empresa')
-	nomeEmpresa = TextField(db_column='nome_empresa')
-	razaoSocial = TextField(db_column='razao_social')
+	nome_empresa = TextField(db_column='nome_empresa')
+	razao_social = TextField(db_column='razao_social')
 	endereco = TextField(db_column='endereco')
 	cep = TextField(db_column='cep')
 	cidade = TextField(db_column='cidade')
@@ -25,36 +25,36 @@ class Empresa(BaseModel):
 	telefone = TextField(db_column='telefone')
 	fax = TextField(db_column='fax')
 	observacao = TextField(db_column='observacao')
-	contatoTitulo = TextField(db_column='contato_titulo')
-	contatoNome = TextField(db_column='contato_nome')
-	contatoCargo = TextField(db_column='contato_cargo')
-	contatoTelefone = TextField(db_column='contato_telefone')
-	contatoCelular = TextField(db_column='contato_celular')
-	contatoEmail = TextField(db_column='contato_email')
-	contatoFinanceiroTitulo = TextField(db_column='contato_financ_titulo')
-	contatoFinanceiroNome = TextField(db_column='contato_financ_nome')
-	contatoFinanceiroCargo = TextField(db_column='contato_financ_cargo')
-	contatoFinanceiroTelefone = TextField(db_column='contato_financ_telefone')
-	contatoFinanceiroCelular = TextField(db_column='contato_financ_celular')
-	contatoFinanceiroEmail = TextField(db_column='contato_financ_email')
-	tipoEnvio = IntegerField(default=0, db_column='tipo_envio')
+	contato_titulo = TextField(db_column='contato_titulo')
+	contato_nome = TextField(db_column='contato_nome')
+	contato_cargo = TextField(db_column='contato_cargo')
+	contato_telefone = TextField(db_column='contato_telefone')
+	contato_celular = TextField(db_column='contato_celular')
+	contato_email = TextField(db_column='contato_email')
+	contato_financeiro_titulo = TextField(db_column='contato_financ_titulo')
+	contato_financeiro_nome = TextField(db_column='contato_financ_nome')
+	contato_financeiro_cargo = TextField(db_column='contato_financ_cargo')
+	contato_financeiro_telefone = TextField(db_column='contato_financ_telefone')
+	contato_financeiro_celular = TextField(db_column='contato_financ_celular')
+	contato_financeiro_email = TextField(db_column='contato_financ_email')
+	tipo_envio = IntegerField(default=0, db_column='tipo_envio')
 	
 	class Meta:
 		table_name = 'clp_tb_empresas'
 		
 class Cliente(BaseModel):
 	id = PrimaryKeyField(db_column='id_cliente')
-	empresaId = IntegerField(db_column='id_empresa')
+	empresa_id = IntegerField(db_column='id_empresa')
 	nome = TextField(db_column='nome_cliente')
 	titulo = TextField(db_column='titulo_cliente')
 	cargo = TextField(db_column='cargo_cliente')
-	dddFixo = TextField(db_column='dddf_cliente')
+	ddd_fixo = TextField(db_column='dddf_cliente')
 	fixo = TextField(db_column='telf_cliente')
-	dddCelular = TextField(db_column='dddc_cliente')
+	ddd_celular = TextField(db_column='dddc_cliente')
 	celular = TextField(db_column='telc_cliente')
 	email = TextField(db_column='email_cliente')
 	observacoes = TextField(db_column='observacoes_cliente')
-	tipoEnvio = IntegerField(db_column='tipo_envio')
+	tipo_envio = IntegerField(db_column='tipo_envio')
 	habilitado = IntegerField(default=1, db_column='Habilitado')
 	empresa = ForeignKeyField(Empresa, db_column='id_empresa')
 	
@@ -66,27 +66,35 @@ class Produto(BaseModel):
 	nome = TextField(db_column='nome_produto')
 	tipo = TextField(db_column='tipo')
 	template = TextField(db_column='template')
+
+
+	@property
+	def assunto(self):
+		if (self.template.find('saude') != -1): 
+  			return 'saude' 
+		return 'seguros' 
+
 	
 	class Meta:
 		table_name = 'clp_tb_produtos'
 
 class Informe(BaseModel):
 	id = PrimaryKeyField(db_column='id_informe')
-	produtoId = IntegerField(db_column='id_produto')
+	produto_id = IntegerField(db_column='id_produto')
 	data = TextField(db_column='data')
 	remetente = TextField(db_column='remetente')
 	titulo = TextField(db_column='titulo')
 	corpo = TextField(db_column='corpo')
 	template = TextField(db_column='template')
-	usuarioId = IntegerField(db_column='id_usuario')
+	usuario_id = IntegerField(db_column='id_usuario')
 	produto = ForeignKeyField(Produto, db_column='id_produto')
 	
 	class Meta:
 		table_name = 'clp_tb_informes'
 
 class InformeLog(BaseModel):
-	informeId = IntegerField(db_column='id_informe')
-	clienteId = IntegerField(db_column='id_cliente')
+	informe_id = IntegerField(db_column='id_informe')
+	cliente_id = IntegerField(db_column='id_cliente')
 	status = TextField(db_column='status')
 	tentativas = IntegerField(db_column='tentativas')
 	cliente = ForeignKeyField(Cliente, db_column='id_cliente')
@@ -112,13 +120,19 @@ def preparaEmail(corpo, Informe):
 #for cliente in Cliente.select():
 #	print('ID [{}] Nome [{}] Empresa[{}]'.format(cliente.id_cliente,cliente.nome_cliente, cliente.empresa.nome_empresa))
 for informeLog in InformeLog.select().where((InformeLog.tentativas < 4) & (InformeLog.status == 'N')):
-	emailSend.send_single_email(informeLog.cliente.email, 
+	if emailSend.send_single_email(informeLog.cliente.email, 
 	html.unescape(informeLog.informe.titulo), 
-	preparaEmail(html.unescape(informeLog.informe.corpo).replace('###','"'), informeLog.informe)
-	)
-	print(html.unescape(informeLog.informe.remetente))
-	print(informeLog.cliente.email)
-	print(html.unescape(informeLog.informe.titulo))
-	print(preparaEmail(html.unescape(informeLog.informe.corpo).replace('###','"'), informeLog.informe))
+	preparaEmail(html.unescape(informeLog.informe.corpo).replace('###','"'), informeLog.informe),
+	informeLog.informe.template + '_t.jpg',
+	'../images/' + informeLog.informe.template + '_t.jpg',
+	'bottom_' + informeLog.informe.produto.assunto + '.jpg',
+	'../images/bottom_' + informeLog.informe.produto.assunto + '.jpg'):
+		print('email enviado')
+		# atualizar o informe que foi enviado.
+		InformeLog.update(status = 'S').where((InformeLog.informe_id == informeLog.informe_id) & (InformeLog.cliente_id == informeLog.cliente_id)).execute()
+		InformeLog.update(tentativas = informeLog.tentativas + 1).where((InformeLog.informe_id == informeLog.informe_id) & (InformeLog.cliente_id == informeLog.cliente_id)).execute()
+	else:
+		print('email nao enviado')
+		InformeLog.update(tentativas = informeLog.tentativas + 1).where((InformeLog.informe_id == informeLog.informe_id) & (InformeLog.cliente_id == informeLog.cliente_id)).execute()
 
 
